13cae93fa5c58cf5b751fb18037d17ec706ad50d
Gonzalovf1996 <gvillalon@alumni.unav.es>
2023-10-17 19:07:36 +0200

    new exercise

---
+++ 2. Intro to Python/1-Basics/Practica/ConversorKM.py
@@ -0,0 +1,10 @@
+#variables
+#Convertir de STR a FLOAT
+km = float(input('Indica los KM para convertir a millas: '))
+#Formula
+mi = km * 0.621371
+
+#Imprimimos la formula 
+print('El numero de KM',km,"es", mi,"millas")
+
+
